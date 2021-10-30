22764a13ac3d15d84a8f90e2f5242deecabe073e
Alex <kolesnikaleksv@gmail.com>
2021-10-30 16:52:57 +0300

    Elements and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//All components start with a capital letter 
+//Component are functions that can return JSX elements
+// It may be a regular function or an arrow functions
+	const Header = () => {
+		return <h1>Hello world!</h1>
+	}
 
-export default App;
+	// const Field = () => {
+	// 	return <input placeholder="write here" type="text"/>
+	// }
+
+	// function Button() {
+	// 	const text = 'log in'
+// We can call functions inside the curly braces
+		// function res() {
+		// 	return 'log in';
+		// }
+		// return <button>{res()}</button>
+		// return <button>{text}</button>
+//inside the element we can put other elements
+	// 	const p = <p>log in</p>
+	// 	return <button>{p}</button>
+
+	// }
+// We can use conditions inside the curly braces
+
+		function Button() {
+			const text = 'log in';
+			const loaded = false;
+			return <button>{loaded ? text : 'Enter'}</button>
+		}
+// We can't use the "if/else" construction
+		
+	// const Field = () => {
+	// 	const holder = 'Enter here';
+	// 	const styledField = {
+	// 		width: '300px'
+	// 	};
+//The long attribute is written to the column
+	// 	return <input 
+	// 	placeholder={holder} 
+	// 	type="text" 
+	// 	style={styledField} />
+	// }
+//Components used to be classes but now they are functions
+//But classes must be ingerited from some kind of components
+//There should be only one main method inside the class: render
+	class Field extends React.Component{
+		render() {
+			const holder = 'Enter here';
+			const styledField = {
+				width: '300px'
+			};
+//And when we need to return something that we wrote: return			
+			return <input 
+			placeholder={holder} 
+			type="text" 
+			style={styledField} />
+		}
+	}
+
+	function App() {
+	return (
+		<div className="App">
+			<Header />
+			<Field />
+			<Button />
+		</div>
+	);
+	}
+// We can do named import
+	// export {Header};
+	export default App;
--- src/index.js
+++ src/index.js
@@ -3,62 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-    // const element = <h2>Hello world</h2>
-
-// const element = React.createElement('h2', {className: 'myClass'}, 'Hello world');
-// in the console: <h2 class="myClass">Hello world</h2>
-// it looks like this in virtualDom:
-    // const element = {
-    //   type: 'h2',
-    //   props:{
-    //     className: 'myClass',
-    //     children: 'Hello world'
-    //   }
-    // }
-
-// !!! import React only in the main index.js file
-
-// the multiline elemen is enclosed parentheses
-// An element must have only one parent
-
-    // const element = (
-    //         <div>
-    //             <h1>Hello world</h1>
-    //             <input type="text" />
-    //             <button>Click</button>
-    //             {/* <button /> if we don't need to put the element inside */}
-    //         </div>
-    // );
-//passing the element to JSX
+// elements are unchanged
+// to change an element it needs to completle redraw
     // const text = 'Hello world'
     //     const element = (
     //             <div>
-    //                 <h1>{text}</h1>
-    //                 {/* <h1>Text: {text}</h1> */}
-    //                 {/* <h1>Text: {2+2}</h1> */}
-    //                 {/* <h1>Text: {new Date()}</h1> //an error */}
-    //                 {/* <h1>Text: {{}}</h1>  // an error */}
-    //                 {/* <h1>Text: {['sdfs', 's323', '234']}</h1> //will return a string */}
+    //                 <h1 className='text'>{text}</h1>
     //                 <input type="text" />
-    //                 <button>Click</button>
+    //                 <label htmlFor=""></label>
+    //                 <button tabIndex={0} >Click</button>
     //             </div>
     //     );
-//Attributes are always written in camelCase
-    const text = 'Hello world'
-        const element = (
-                <div>
-                    <h1 className='text'>{text}</h1>
-                    <input type="text" />
-                    <label htmlFor=""></label>
-                    <button tabIndex={0} >Click</button>
-                </div>
-        );
 
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  element,
+    <App />
+  //element
+  ,
   document.getElementById('root')
 );
