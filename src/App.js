787fa78596434f1d7dab5770b71d9c531770d58b
Alex <kolesnikaleksv@gmail.com>
2021-11-02 13:36:23 +0200

    Events in React

--- src/App.js
+++ src/App.js
@@ -2,129 +2,139 @@ import React from 'react';
 import { Component } from 'react';
 import './App.css';
 
-// Components state
-// state in the classes
+//Events in React
+// // onChange == onInput
+// 	class WhoAmI extends Component {
+// 		constructor(props) {
+// 			super(props);
+// 			this.state = {
+// 				years: 28,
+// 				text: 'click here',
+// 				position: ''
+// 			}
+// 		}
 
-	// function WhoAmI({name, surname, link}) {
-	// 	return(
-	// 		<div>
-	// 			<h1>My name is {name()}, surname - {surname}</h1>
-	// 			<a href={link}>My profile</a>
-	// 		</div>
-	// 	)
-	// }
-//let's rewrite our function into a class
-//We will also see how to 'props' works inside class component
-	// class WhoAmI extends Component {
-//when a class component is called, it create its instance:<WhoAmI/>
-//To pass a props to class instances, we need to create a constructor
-		// constructor(props) {
-// So we can use 'props', we prescribe keyword 'super' and pass 'props'
-			// super(props);
-//Now this.props appear in every instance
-		// }
-//We do not need an empty constructor and can be deleted
-//but if there are additional props in it, it can be returned
-		// constructor(props) {
-		// 	super(props);
-//we can't to change read-only 'props',
-// but we can create state and changed it
-	// 		this.state = {
-	// 			years: 28
-	// 		}
-	// 	}
-	// 	render() {
-	// 		const {name, surname, link} = this.props;
-	// 		return(
-	// 			<div>
-	// 				<h1>My name is {name}, surname - {surname} age={this.state.years}</h1>
-	// 				<a href={link}>My profile</a>
-	// 			</div>
-	// 		)
-	// 	}
-	// }
-
-//create a button with the event
-//create a method to change the state	
-	// class WhoAmI extends Component {
-	// 	constructor(props) {
-	// 		super(props);
-	// 		this.state = {
-	// 			years: 28
-	// 		}
-	// 	}
-// create a method nextYear
-//It should be an arrow function
-		// nextYear = () => {
-		// 	console.log('+++');
-		// } 
-//How to change the 'state' state correctly
-//'state' cannot be changed directly like this:
-	// nextYear = () => {
-	// 	console.log('+++');
-	// 	this.state.years++//You can't do that !!!!!
-	// } 
 // 	nextYear = () => {
-// 		console.log('+++');
-// 		this.setState({
-// //This way we also change the state directly
-// 			years: ++this.state.years// you can't do that !!!
-// 		})
+// 		this.setState(state => ({
+// 			years: state.years + 1 
+// 		}))
 // 	}
-//Now the right way
-	// nextYear = () => {
-	// 	console.log('+++');
-	// 	this.setState({
-	// 		years: this.state.years + 1
-	// 	})
-	// }
-//To make this code asynchronous we need to pass there a callback function
-	// 	nextYear = () => {
-	// 		console.log('+++');
-	// 		this.setState(state => ({
-	// 			years: state.years + 1 // we need to remove 'this.'
-	// 		}))
-	// 	}
-	// 	render() {
-	// 		const {name, surname, link} = this.props;
-	// 		return(
-	// 			<div>
-	// 				<button onClick={this.nextYear}>click here</button>
-	// 				<h1>My name is {name}, surname - {surname} age={this.state.years}</h1>
-	// 				<a href={link}>My profile</a>
-	// 			</div>
-	// 		)
-	// 	}
-	// }
 
-//If we have several properties, the setState function will only
-//change what it was told, the rest of the states will remain intact
+// 	CommitInputChange = (e) => {
+// 		//console.log(e.target); // we see where it is works - in input
+// 		//console.log(e.target.value);// we see the value of input
+// 		this.setState ({
+// 			position: e.target.value
+// 		})
+// 		console.log(this.state.position);
+// 	}
+
+//When the event is triggered. the call context is lost
+//If we rewrite an arrow function, everything is break
+//There are three ways to avoid this problem
+//1 Through the bind construction
+// we essentially pass this method to the class
+//2 always use arrow functions(how it was originally)
+//3 call using arrow function in event
+		
+// 	class WhoAmI extends Component {
+// 		constructor(props) {
+// 			super(props);
+// 			this.state = {
+// 				years: 28,
+// 				text: 'click here',
+// 				position: ''
+// 			}
+// 			// this.nextYear = this.nextYear.bind(this)//The first way
+// 		}
+
+// 	nextYear() {
+// 		this.setState(state => ({
+// 			years: state.years + 1 
+// 		}))
+// 	}
+
+// 	CommitInputChange = (e) => {
+// 		this.setState ({
+// 			position: e.target.value
+// 		})
+// 		console.log(this.state.position);
+// 	}
+// 	render() {
+// 		const {name, surname, link} = this.props;
+// 		const {years, position} = this.state;
+// 		return(
+// 			<div>
+// 				 <button onClick={() => this.nextYear()}>{this.state.text}</button>{/* The third way */}
+// 				<h1>My name is {name}, 
+// 					surname - {surname},  
+// 					age - {years},  
+// 					position - {position} </h1>
+// 				<a href={link}>My profile</a>
+// 				<form >
+// 					<span>Written here</span>
+// 					<input onChange={this.CommitInputChange} />
+// 				</form>
+// 			</div>
+// 		)
+// 	}
+// }
+// 	function App() {
+// 		return (
+// 			<div className="App">
+// 				<WhoAmI name='John' surname='Smith' link='facebook.com' />
+// 				<WhoAmI name='Alex' surname='Smoothy' link='linkedin.com' />
+// 			</div>
+// 		);
+// 	}
+
+// export default App;
+
+//Using arguments in event handlers
+//To pass arguments we create an arrow function and be sure to pass (e) and 
+// other arguments as needed
 	class WhoAmI extends Component {
 		constructor(props) {
 			super(props);
 			this.state = {
 				years: 28,
-				text: 'click here'
+				text: 'click here',
+				position: ''
 			}
 		}
 
 	nextYear = () => {
-		// console.log('+++');
 		this.setState(state => ({
 			years: state.years + 1 
 		}))
 	}
+
+	CommitInputChange = (e, text) => {
+		console.log(text);
+		this.setState ({
+			position: e.target.value
+		})
+		console.log(this.state.position);
+	}
 	render() {
 		const {name, surname, link} = this.props;
+		const {years, position} = this.state;
 		return(
 			<div>
 				<button onClick={this.nextYear}>{this.state.text}</button>
-				<h1>My name is {name}, surname - {surname} age={this.state.years}</h1>
+				<h1>My name is {name}, 
+					surname - {surname},  
+					age - {years},  
+					position - {position} </h1>
 				<a href={link}>My profile</a>
+				<form >
+					<span>Written here</span>
+					<input onChange={(e) => {this.CommitInputChange(e, 'some text')}} />
+				</form>
 			</div>
 		)
 	}
-}
+	}
 	function App() {
 		return (
 			<div className="App">
@@ -134,64 +144,4 @@ import './App.css';
 		);
 	}
 
-export default App;
-
-// The task from teacher on the codepan is complete
-class App extends React.Component {
-	constructor(props) {
-	  super(props);
-	  this.state = {
-		counter: this.props.counter
-	  }
-	}
-	
-	incr = () => {
-	  if(this.state.counter < 50) {
-		 this.setState( {
-		 counter: this.state.counter + 1
-	  });
-	  } 
-	}
-	decr = () => {
-	  if(this.state.counter > -50) {
-		this.setState({
-		  counter: this.state.counter - 1
-		});
-	  }
-	}
-	getRandom = () => {
-	   let randomNum = Math.floor(Math.random() * (50 - -50)) + -50;
-	  this.setState({
-		counter: randomNum
-	  })
-	}
-	getReset = () => {
-	  this.setState({
-		counter: this.props.counter
-	  })
-	}
-	// Используйте только стрелочную форму методов
-	// Почему? Подробный ответ будет в следующем уроке
-	
-	render() {
-	  const {counter} = this.state;
-	  return (
-		<div class="app">
-		  <div class="counter">{counter}</div>
-		  <div class="controls">
-			<button onClick={this.incr}>INC</button>
-			<button onClick={this.decr} >DEC</button>
-			<button onClick={this.getRandom} >RND</button>
-			<button onClick={this.getReset} >RESET</button>
-		  </div>
-		</div>
-	  )
-	}
-  }
-  
-  ReactDOM.render(<App counter={10}/>, document.getElementById('app'));
-  
-  // 1) Начальное значение счетчика должно передаваться через props
-  // 2) INC и DEC увеличивают и уменьшают счетчик соответственно на 1. Без ограничений, но можете добавить границу в -50/50. По достижению границы ничего не происходит
-  // 3) RND изменяет счетчик в случайное значение от -50 до 50. Конструкцию можете прогуглить за 20 секунд :) Не зависит от предыдущего состояния
-  // 4) RESET сбрасывает счетчик в 0 или в начальное значение из пропсов. Выберите один из вариантов
+	export default App;
